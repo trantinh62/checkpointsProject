02761275b4a39a79a9c07f8f99260cd03e85b87c
Van Hung <VanHung14>
2022-09-06 16:43:48 +0700

    fix bug update profile, add toast

--- my-app/src/Components/ChangePassword/ChangePassword.js
+++ my-app/src/Components/ChangePassword/ChangePassword.js
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import axiosClient from "../../Api/axiosClient";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const change_pass_url = "/api/profile/change_password";
 
@@ -34,10 +35,30 @@ function ChangePassword() {
           Accept: "application/json",
         },
       });
-      navigate("/profile", { replace: true });
+      navigate("/changepassword", { replace: true });
       navigate(0);
+      toast.success("Update successful!", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     } catch (err) {
       console.log("err", err);
+      if (err.response.status === 422) {
+        toast.error("The password must be at least 8 characters.", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
     }
   };
   return (
@@ -46,44 +67,42 @@ function ChangePassword() {
         <div className="row">
           <div className="col"></div>
           <div className="col-6">
-            <form className="form-profile">
+            <Form className="form-profile" onSubmit={handleSumbit}>
               <h1>Change password</h1>
-              <Form onSubmit={handleSumbit}>
-                <Form.Group className="mb-3" controlId="formBasicOldPass">
-                  <Form.Label>Old password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    name="old_password"
-                    onChange={onChangeInput}
-                    placeholder="Old password"
-                  />
-                </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicOldPass">
+                <Form.Label>Old password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="old_password"
+                  onChange={onChangeInput}
+                  placeholder="Old password"
+                />
+              </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicNewPass">
-                  <Form.Label>New password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    name="password"
-                    onChange={onChangeInput}
-                    placeholder="New password"
-                  />
-                </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicNewPass">
+                <Form.Label>New password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  onChange={onChangeInput}
+                  placeholder="New password"
+                />
+              </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasiConfirmPass">
-                  <Form.Label>Confirm password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    name="password_confirm"
-                    onChange={onChangeInput}
-                    placeholder="Confirm password"
-                  />
-                </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasiConfirmPass">
+                <Form.Label>Confirm password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password_confirm"
+                  onChange={onChangeInput}
+                  placeholder="Confirm password"
+                />
+              </Form.Group>
 
-                <Button variant="primary" type="submit">
-                  Change
-                </Button>
-              </Form>
-            </form>
+              <Button variant="primary" type="submit">
+                Change
+              </Button>
+            </Form>
           </div>
           <div className="col"></div>
         </div>
--- my-app/src/Components/Profile/Profile.jsx
+++ my-app/src/Components/Profile/Profile.jsx
@@ -2,17 +2,16 @@ import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import axiosClient from "../../Api/axiosClient";
-import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import "./Profile.css";
 const profile_url = "/api/profile/detail";
 const update_url = "/api/profile";
 
 const Profile = () => {
-  const navigate = useNavigate();
-
   const [dataProfile, setDataProfile] = useState({
     last_name: "",
     first_name: "",
+    age: "",
     address: "",
     phone: "",
   });
@@ -53,11 +52,28 @@ const Profile = () => {
           Accept: "application/json",
         },
       });
+      console.log(response);
       sessionStorage.setItem("sessionUser", response.data.data.first_name);
-      navigate("/profile", { replace: true });
-      navigate(0);
+      toast.success("Update successful!", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     } catch (err) {
       console.log("err", err);
+      toast.error("Update failed!", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
   };
 
@@ -67,68 +83,77 @@ const Profile = () => {
         <div className="row">
           <div className="col"></div>
           <div className="col-6">
-            <form className="form-profile">
+            <Form className="form-profile" onSubmit={handleSumbit}>
               <h1>Profile</h1>
-              <Form onSubmit={handleSumbit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control
-                    type="text-form"
-                    readOnly
-                    defaultValue={dataProfile.email}
-                    placeholder="Enter email"
-                  />
-                </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="text-form"
+                  readOnly
+                  defaultValue={dataProfile.email}
+                  placeholder="Enter email"
+                />
+              </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicFirstname">
-                  <Form.Label>Firstname</Form.Label>
-                  <Form.Control
-                    type="text-form"
-                    name="first_name"
-                    defaultValue={dataProfile.first_name}
-                    onChange={onChangeInput}
-                    placeholder="FirstName"
-                  />
-                </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicFirstname">
+                <Form.Label>Firstname</Form.Label>
+                <Form.Control
+                  type="text-form"
+                  name="first_name"
+                  defaultValue={dataProfile.first_name}
+                  onChange={onChangeInput}
+                  placeholder="FirstName"
+                />
+              </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicLastname">
-                  <Form.Label>Lastname</Form.Label>
-                  <Form.Control
-                    type="text-form"
-                    name="last_name"
-                    defaultValue={dataProfile.last_name}
-                    onChange={onChangeInput}
-                    placeholder="Lastname"
-                  />
-                </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicLastname">
+                <Form.Label>Lastname</Form.Label>
+                <Form.Control
+                  type="text-form"
+                  name="last_name"
+                  defaultValue={dataProfile.last_name}
+                  onChange={onChangeInput}
+                  placeholder="Lastname"
+                />
+              </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicAddress">
-                  <Form.Label>Address</Form.Label>
-                  <Form.Control
-                    type="text-form"
-                    name="address"
-                    onChange={onChangeInput}
-                    defaultValue={dataProfile.address}
-                    placeholder="Address"
-                  />
-                </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicAge">
+                <Form.Label>Age</Form.Label>
+                <Form.Control
+                  type="text-form"
+                  name="age"
+                  onChange={onChangeInput}
+                  defaultValue={dataProfile.age}
+                  placeholder="Age"
+                />
+              </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPhone">
-                  <Form.Label>Phone</Form.Label>
-                  <Form.Control
-                    type="text-form"
-                    name="phone"
-                    onChange={onChangeInput}
-                    defaultValue={dataProfile.phone}
-                    placeholder="Phone"
-                  />
-                </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicAddress">
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                  type="text-form"
+                  name="address"
+                  onChange={onChangeInput}
+                  defaultValue={dataProfile.address}
+                  placeholder="Address"
+                />
+              </Form.Group>
 
-                <Button variant="primary" type="submit">
-                  Update
-                </Button>
-              </Form>
-            </form>
+              <Form.Group className="mb-3" controlId="formBasicPhone">
+                <Form.Label>Phone</Form.Label>
+                <Form.Control
+                  type="text-form"
+                  name="phone"
+                  onChange={onChangeInput}
+                  defaultValue={dataProfile.phone}
+                  placeholder="Phone"
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Update
+              </Button>
+            </Form>
           </div>
           <div className="col"></div>
         </div>
