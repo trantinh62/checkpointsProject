22e10424595c3a3fdb806b919b85fa73f017fd1f
Van Hung <VanHung14>
2022-09-15 01:09:25 +0700

    merge develop to feature/assign

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,27 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-<<<<<<< HEAD
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
   <React.StrictMode>
     <BrowserRouter>
       <App></App>
     </BrowserRouter>
   </React.StrictMode>
-=======
-import { StrictMode } from "react";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App></App>
-    </BrowserRouter>
-  </StrictMode>
->>>>>>> 7d0c465 (checkpointFe)
 );
 
 reportWebVitals();
